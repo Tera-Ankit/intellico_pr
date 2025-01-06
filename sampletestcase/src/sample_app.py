83c2022b9082f7b987a9b54ff613fdc56022c449
nizmaparvin <nizmap@terawe.com>
2025-01-06 13:18:00 +0530

    Add sampletestcase folder to the project

---
+++ sampletestcase/src/sample_app.py
@@ -0,0 +1,7 @@
+# sample_app.py
+def add(a, b):
+    """Returns the sum of a and b."""
+    return a + b
+def subtract(a, b):
+    """Returns the result of subtracting b from a."""
+    return a - b
